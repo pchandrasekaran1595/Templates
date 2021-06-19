e9275c9b6f0243abbaceb2c4f1977aaf2d2b414a
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-06-19 12:20:43 +0530

    Create Simple Video Processing.py

---
+++ Video Handling/Simple Video Processing.py
@@ -0,0 +1,40 @@
+import os
+import cv2
+import sys
+import getpass
+import numpy as np
+
+from cam_and_vid import Handler
+
+def handle_video(path=None):
+    width, height, fps = 640, 480, 30
+
+    args_1 = "--width"
+    args_2 = "--height"
+
+    if args_1 in sys.argv:
+        width = int(sys.argv[sys.argv.index(args_1) + 1])
+    if args_2 in sys.argv:
+        height = int(sys.argv[sys.argv.index(args_2) + 1])
+    
+    if not path:
+        CaptureObject = Handler(device_id=0, width=width, height=height, fps=fps)
+        CaptureObject.start()
+    else:
+        CaptureObject = Handler(path=path,  width=width, height=height, fps=fps)
+        CaptureObject.start()
+    
+    while True:
+        ret, frame = CaptureObject.getframe()
+        if ret:
+            """
+                Process the frame
+            """
+            cv2.imshow("Blended", frame)
+            if cv2.waitKey(1) == ord("q"):
+                break
+        else:
+            break
+    
+    CaptureObject.stop()
+    cv2.destroyAllWindows()
